cf4b63800a7ae5ba14161ab738155367d410bcfe
chhayang <chhayangpatel@gmail.com>
2019-04-23 10:50:49 -0400

    Added openshift app setup script

---
+++ openshift.sh
@@ -0,0 +1,34 @@
+## login to server
+oc login -u developer -p developer --server https://172.17.0.43:8443 --insecure-skip-tls-verify
+
+## create project 
+
+oc new-project testdocker
+
+## Create App within project
+
+oc new-app https://github.com/chhayang/TestDocker.git --strategy=docker
+
+## check build logs
+
+oc logs -f bc/testdocker
+
+## check build status
+
+oc status
+
+## expose service
+
+oc expose service testdocker
+
+## url 
+
+oc get route
+
+
+
+
+
+
+
+
